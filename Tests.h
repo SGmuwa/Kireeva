614f2c5e9dc021baf6c83d51424d09c11808e74f
SG_Muwa <motherlode.muwa@gmail.com>
2018-07-25 09:09:07 +0400

    Упрощение тестирования
    Внешний вид был пояснён.

--- Tests.h
+++ Tests.h
@@ -7,45 +7,50 @@ void runTest()
 	unsigned path;
 	unsigned(*getMaxPath)(size_t, unsigned*) = getMaxPath_Maria;
 
-	x = new unsigned[1]{ 0 };
+	// Инцилизация входных данных в формате {y, x1, x2, x3, ...} или {x0, x1, x2, x3}.
+	x = new unsigned[1]{ 0 }; 
+	// Выполнение программы.
 	path = getMaxPath(1, x);
+	// Узнаём, является ли ответ не правильным.
 	if (path != 0)
-		printf("0 error. %u %u\n", x[0], path);
+		// Ответ оказался не правильным. Выводим отчёт: номер теста; какой результат получился.
+		printf("0 error. %u\n", path);
+	// Деинцилизируем входные данные, чтобы очистить оперативную память.
 	delete x;
 
 	x = new unsigned[2]{ 0, 1 };
 	path = getMaxPath(2, x);
 	if (path != 1)
-		printf("1 error. %u %u\n", x[1], path);
+		printf("1 error. %u\n", path);
 	delete x;
 
 	x = new unsigned[3]{ 0, 1, 2 };
 	path = getMaxPath(3, x);
 	if (path != 2)
-		printf("2 error. %u %u\n", x[2], path);
+		printf("2 error. %u\n", path);
 	delete x;
 
 	x = new unsigned[2]{ 0, 2 };
 	path = getMaxPath(2, x);
 	if (path != 2)
-		printf("3 error. %u %u\n", x[1], path);
+		printf("3 error. %u\n", path);
 	delete x;
 
 	x = new unsigned[4]{ 0, 2, 4, 2 };
 	path = getMaxPath(4, x);
 	if (path != 4)
-		printf("4 error. %u %u\n", x[3], path);
+		printf("4 error. %u\n", path);
 	delete x;
 
 	x = new unsigned[4]{ 1, 2, 4, 2 };
 	path = getMaxPath(4, x);
 	if (path != 3)
-		printf("5 error. %u %u\n", x[3], path);
+		printf("5 error. %u\n", path);
 	delete x;
 
 	x = new unsigned[2]{ 10, 2 };
 	path = getMaxPath(2, x);
 	if (path != 8)
-		printf("6 error. %u %u\n", x[1], path);
+		printf("6 error. %u\n", path);
 	delete x;
 }
